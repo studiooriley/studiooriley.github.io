999bf25dbeeb9c3073401a3477b80977ceabad69
studiooriley <studiooriley@gmail.com>
2022-05-05 15:52:38 -0700

    Removed Contentful API

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -20,17 +20,3 @@ accordionItemHeaders.forEach(accordionItemHeader => {
 });
 
 // Contentful API
-var contentful = require('contentful');
-
-var client = contentful.createClient({
-  space: '<izdjqwxzyzcv>',
-  accessToken: '<12jAfO_yvwGUFdfsNdRVK-XL3ylBGZdswtyhrgfnuw8>',
-});
-
-client.getEntry('<18hcpQ5DjAG8JWCokGWqGU>').then(function (entry) {
-  // logs the entry metadata
-  console.log(entry.sys);
-
-  // logs the field with ID title
-  console.log(entry.fields.productName);
-});
