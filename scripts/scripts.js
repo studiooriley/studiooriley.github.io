d3d0cbef2e51a23661654a87354ea3d48fa37537
studiooriley <studiooriley@gmail.com>
2022-05-09 11:38:26 -0700

    Updated Contentful API with Space ID and Token and Entry ID (product ID?)

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -22,11 +22,11 @@ accordionItemHeaders.forEach(accordionItemHeader => {
 // Contentful API
 
 var contentfulClient = contentful.createClient({
-  accessToken: '0e3ec801b5af550c8a1257e8623b1c77ac9b3d8fcfc1b2b7494e3cb77878f92a',
-  space: 'wl1z0pal05vy'
+  accessToken: '12jAfO_yvwGUFdfsNdRVK-XL3ylBGZdswtyhrgfnuw8',
+  space: 'isdjqwxzyzcv'
 })
 
-var PRODUCT_CONTENT_TYPE_ID = '2PqfXUJwE8qSYKuM0U6w8M'
+var PRODUCT_CONTENT_TYPE_ID = '18hcpQ5DjAG8JWCokGWqGU'
 
 var container = document.getElementById('content')
 
